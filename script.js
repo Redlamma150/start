71409a6143199ea048354052dbf5b7e67953e4e9
rafael <rafael.k999s@gmail.com>
2024-11-10 18:48:20 +0200

    deleted a mistake

--- script.js
+++ script.js
@@ -4,9 +4,3 @@ h1.addEventListener('click', function(e) {
     alert('This is a click event on the h1 heading');
 });
 
-p{
-    font-size: 20px;
-    text-align: center;
-    background-color: lightgray;
-    color: pink;
-}
