ba56adf9ea0fb46e7c2e9e3ac27affd44a14711d
rafael <rafael.k999s@gmail.com>
2024-11-10 18:44:06 +0200

    added p

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ h1.addEventListener('click', function(e) {
     e.preventDefault();
     alert('This is a click event on the h1 heading');
 });
+
+p{
+    font-size: 20px;
+    text-align: center;
+    background-color: lightgray;
+    color: pink;
+}
